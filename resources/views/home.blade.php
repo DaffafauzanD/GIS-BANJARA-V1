7df8dce8f7c804c39f938c7ecb332bb36d5e81bf
DaffafauzanD <daffafauzand24@gmail.com>
2024-12-08 20:40:36 +0700

    add component for nav and modify front edn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,70 +13,11 @@
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
     </head>
     <body>
-        <nav class="navbar navbar-default nav-fixed-top" role="navigation" id="app-nav-bar" >
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">GIS - BJR</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active">
-                        <a href="/home">Home</a>
-                    </li>
-                    <li>
-                        <a href="#">Browse</a>
-                    </li>
-
-                </ul>
-                <ul class="nav navbar-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Application<b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="#">Setting</a>
-                            </li>
-                            <li>
-                                <a href="#">Manage User</a>
-                            </li>
-
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Master<b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="#">Point</a>
-                            </li>
-                            <li>
-                                <a href="#">Category</a>
-                            </li>
-
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-
-                </ul>
-                
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="login.html">Login</a>
-                    </li>
-
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </nav>
+        
+        <x-navbar></x-navbar>
 
         <div id="map-canvas" ></div>
+        
         <div class="visible-lg visible-md">
             <div id="search-box">
                 <form class="form-inline" role="form">
