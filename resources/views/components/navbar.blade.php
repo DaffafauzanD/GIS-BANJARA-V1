2bd319bcc8c50699b8e28d72b69f4ab97b5f5aa5
DaffafauzanD <daffafauzand24@gmail.com>
2025-01-12 21:42:11 +0700

    Update navbar layout and styles; enhance responsiveness and add new CSS rules

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,62 +1,47 @@
-<nav class="navbar navbar-default nav-fixed-top" role="navigation" id="app-nav-bar" >
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+<nav class="navbar navbar-expand-lg fixed-top" id="app-nav-bar">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <a class="navbar-brand fs-2" href="#">GIS - BJR</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">GIS - BJR</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse navbar-ex1-collapse">
-        <ul class="nav navbar-nav">
-            <li class="active">
-                <a href="/home">Home</a>
-            </li>
-            <li>
-                <a href="#">Browse</a>
-            </li>
-
-        </ul>
-        <ul class="nav navbar-nav">
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Application<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="#">Setting</a>
-                    </li>
-                    <li>
-                        <a href="#">Manage User</a>
-                    </li>
-
-                </ul>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Master<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="#">Point</a>
-                    </li>
-                    <li>
-                        <a href="#">Category</a>
-                    </li>
-
-                </ul>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-
-        </ul>
-        
-        <ul class="nav navbar-nav navbar-right">
-            <li>
-                <a href="login.html">Login</a>
-            </li>
-
-        </ul>
-    </div><!-- /.navbar-collapse -->
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link fs-5" href="/home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-5" href="#">Browse</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link fs-5 dropdown-toggle" href="#" id="applicationDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Application
+                    </a>
+                    <ul class="dropdown-menu rounded" aria-labelledby="applicationDropdown">
+                        <li><a class="dropdown-item" href="#">Setting</a></li>
+                        <li><a class="dropdown-item" href="#">Manage User</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link fs-5 dropdown-toggle" href="#" id="masterDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Master
+                    </a>
+                    <ul class="dropdown-menu rounded" aria-labelledby="masterDropdown">
+                        <li><a class="dropdown-item" href="#">Point</a></li>
+                        <li><a class="dropdown-item" href="#">Category</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-5" href="#">About</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link fs-5" href="login.html">Login</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
